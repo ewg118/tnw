5f61003958de2237a4b96e5ead1d2cbdf2155b4e
Ethan Gruber <ewg4xuva@gmail.com>
2021-05-19 13:48:26 -0400

    first prototype

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ if($sec) $title .= "$sec Seconds ";
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">//</script>
         <script type="text/javascript" src="js/tnw.js">//</script>
+         <link rel="stylesheet" href="css/style.css" />
         <title><?php echo $title ?></title>
 
 		<meta property="og:url" content="http://tuesdaynightworlds.org"/>
@@ -47,12 +48,37 @@ if($sec) $title .= "$sec Seconds ";
 		<meta name="twitter:image" content="http://tuesdaynightworlds.org/images/TNW_WORLD_19-01.jpg"/>
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Hello world!</h1>
+        <div class="container-fluid" style="height:100%">
+            <div class="row" style="height:50%">
+                <div class="col-md-12 text-center" style="height:100%">
+                    <img src="images/TNW_WORLD_19-01.png" alt="TNW logo" style="height:100%;max-width:100%"/>
                 </div>
             </div>
+            <div class="row">
+            	<div class="col-md-12 text-center">
+            		<div>Next TNW begins in</div>
+            		<?php if ($day > 0) { 
+            		    echo '<div class="timer"><span id="day">' . $day . '</span><br/>days</div>'; 
+            		} ?>
+            		<?php if ($hr > 0) { 
+            		    echo '<div class="timer"><span id="hr">' . $hr . '</span><br/>hours</div>'; 
+            		} ?>
+            		<?php if ($min > 0) { 
+            		    echo '<div class="timer"><span id="min">' . $min . '</span><br/>minutes</div>'; 
+            		} ?>
+            		<div class="timer"><span id="sec"><?php echo $sec; ?></span><br/>seconds</div>            		
+            	</div>
+            	<div class="col-md-12 text-center">
+            		<span>Tuesday Night Worlds is the Charlottesville area's premier grassroots race-pace group ride.<br/>TNW meets near the Earlysville Post Office every Tuesday at 6pm from mid-March to late-October</span>
+            		<br/>•<br/>
+            		<a href="https://www.strava.com/segments/850343">Strava Segment</a>
+            		<br/>•<br/>
+            		<span>Logos by <a href="https://www.cutawayusa.com/">Cutaway</a>. <a href="https://www.cutawayusa.com/collections/tnw">Buy the jersey</a></span>
+            	</div>
+            </div>
+        </div>
+        <div class="hidden">
+        	<span id="nextTime"><?php echo date(DATE_W3C, $next) ?></span>
         </div>
     </body>
 </html>
--- js/tnw.js
+++ js/tnw.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {	
-    
+	var now = new Date($('#now').text());
+	var next = new Date($('#nextTime').text());
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+	  // Find the distance between now and the count down date
+	var now = new Date().getTime();
+	  var rem = next - now;
+
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(rem / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((rem % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((rem % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((rem % (1000 * 60)) / 1000);
+
+	  //repopulate countdown
+	  if ($('#day').length){
+		  $('#day').text(days);
+	  }
+	  if ($('#hr').length){
+		  $('#hr').text(hours);
+	  }
+	  if ($('#min').length){
+		  $('#min').text(minutes);
+	  }	  
+	  if ($('#sec').length){
+		  $('#sec').text(seconds);
+	  }
+	  // If the count down is finished, write some text
+	  if (rem < 0) {
+		  window.location.reload(true)
+	  }
+	}, 1000);
 });
